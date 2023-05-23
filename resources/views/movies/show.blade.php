2892436755c4d86bab63aa6a19765a3372a628aa
nicolozibra1 <nico.zibra@gmail.com>
2023-05-23 17:26:30 +0200

    upgrade-layout (movie-show)

--- resources/views/movies/show.blade.php
+++ resources/views/movies/show.blade.php
@@ -1,13 +1,25 @@
 @extends('layouts.app')
 
 @section('content')
-    <section class="container">
-        <h1>{{$movie->title}}</h1>
-        <span>{{$movie->original_title}}</span>
-        <span>{{$movie->nationality}}</span>
-        <span>{{$movie->date}}</span>
-        <span>{{$movie->vote}}</span>
-        <img src="{{$movie->image}}" alt="{{$movie->title}}">
-
+    <div class="box-logo m-auto" id="show-page">
+        <img src="/img/logo.png" alt="">
+    </div>
+    <section class="container d-flex flex-column align-items-between mt-5" id="show-movie">
+        <div class="row d-flex">
+            <div class="col-6 d-flex justify-content-center w-50">
+                <div class="box-image">
+                    <img src="{{$movie->image}}" alt="{{$movie->title}}">
+                </div>
+            </div>
+            <div class="col-6 w-50 d-flex flex-column gap-3">
+                <h1 class="text-uppercase fw-bold">{{$movie->title}}</h1>
+                <hr>
+                <span><span class="fw-lighter">Original title: </span>{{$movie->original_title}}</span>
+                <span><span class="fw-lighter">Nationality: </span>{{$movie->nationality}}</span>
+                <span><span class="fw-lighter">Date release: </span>{{$movie->date}}</span>
+                <span><span class="fw-lighter">Vote: </span>{{$movie->vote}}</span>
+            </div>
+        </div>
+        <img class="art-bg" src="/img/popcorn.png" alt="">
     </section>
 @endsection
