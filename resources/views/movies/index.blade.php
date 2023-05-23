b3f1a4e227c5c02611f2832aa2d13302307f6303
nicolozibra1 <nico.zibra@gmail.com>
2023-05-23 16:06:25 +0200

    upgrade-layout-movies-index

--- resources/views/movies/index.blade.php
+++ resources/views/movies/index.blade.php
@@ -1,12 +1,26 @@
 @extends('layouts.app')
 
 @section('content')
-    <section class="container">
-        <h1>Home Page</h1>
-        <ul>
-            @foreach ($movies as $movie )
-                <li><a href="{{route('movies.show', ['id' => $movie->id])}}">{{$movie ->title}}</a></li>
+    <section class="container d-flex flex-column align-items-center" id="movies-index">
+        <div class="box-logo">
+            <img src="/img/logo.png" alt="">
+        </div>
+        <h1>Movies List</h1>
+        <div class="row d-flex">
+            @foreach ($movies as $movie)
+            <div class="col-3">
+                <a href="{{route('movies.show', ['id' => $movie->id])}}">
+                    <div class="card">
+                        <div class="card-img">
+                            <img src="{{$movie->image}}" alt="">
+                        </div>
+                        <div class="card-body">
+                            <h6 class="text-center">{{$movie ->title}}</h6>
+                        </div>
+                    </div>
+                </a>
+            </div>
             @endforeach
-        </ul>
+        </div>
     </section>
 @endsection
